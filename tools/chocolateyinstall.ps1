5414779e29da9b8e66a7f581803463b996e17add
Alan Płócieniak <alan-null@outlook.com>
2019-08-13 07:55:20 +0200

    Addressed reviewer suggestions

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,21 +2,17 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://app.prntscr.com/build/setup-lightshot.exe'
-$url64 = 'https://app.prntscr.com/build/setup-lightshot.exe'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'exe'
     url            = $url
-    url64bit       = $url64
 
     softwareName   = 'lightshot.install*'
 
     checksum       = 'ACDFBB45BFED06F1BC92888D5D1A36CE'
     checksumType   = 'md5'
-    checksum64     = 'ACDFBB45BFED06F1BC92888D5D1A36CE'
-    checksumType64 = 'md5'
 
     validExitCodes = @(0, 3010, 1641)
     silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
